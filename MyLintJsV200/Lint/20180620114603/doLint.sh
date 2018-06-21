0c416f58eb250af225c43a0d64d1ef66eb9749ee
volwlx <zhengvolcano@163.com>
2018-06-21 13:28:32 +0800

    exmples to .zip

--- MyLintJsV200/Lint/20180620114603/doLint.sh
+++
@@ -1,5 +0,0 @@
-echo --------lint start--------
-sh /d/web/MyLintJsV200/Lint/lintPre.sh
-jshint --reporter=/d/web/MyLintJsV200/Lint/myreporter.jsonV2.js ./zc
-sh /d/web/MyLintJsV200/Lint/lintAfter.sh
-echo -------- lint end--------
